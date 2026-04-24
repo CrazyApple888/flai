b01e66160df0254adf6050de9ceb7f8b8969839f
aisachenko <isach.gu@gmail.com>
2026-04-24 15:48:23 +0300

    fix build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+    repositories {
+        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
